c3103d4f5d8d9aaa255187d9eb3e0ac624223e97
Divyam <nihaldivyam@gmail.com>
2024-07-10 01:51:57 +0530

    test

--- logical-backup/dump.sh
+++ logical-backup/dump.sh
@@ -6,7 +6,7 @@ PG_BIN=$PG_DIR/$PG_VERSION/bin
 export PGHOST="output-etl-db.modeling.svc.cluster.local"
 export PGPORT="5432"
 
-pg_dumpall | pigz /tmp/logicalbakup.sql.gz
+pg_dumpall > /backup/backup.sql
 
-aws s3 cp s3://bw7-k8s-production-postgres-backup/spilo/output-etl-db/"$LOGICAL_BACKUP_S3_BUCKET_SCOPE_SUFFIX"/
+aws s3 cp /backup/backup.sql s3://bw7-k8s-production-postgres-backup/spilo/output-etl-db/"$LOGICAL_BACKUP_S3_BUCKET_SCOPE_SUFFIX"/
 
