99d4a9290c69d472e5c60a85c9062565619440cb
Bhavishya <bhavishya@obmondo.com>
2026-02-20 12:52:14 +0530

    add aws upload function in the new script

--- mariadb-logical-backup/mariadb-dump-file2.sh
+++ mariadb-logical-backup/mariadb-dump-file2.sh
@@ -7,6 +7,14 @@ IFS=$'\n\t'
 ERRORCOUNT=0
 CLUSTER_NAME=${CLUSTER_NAME_LABEL:-"mariadb-cluster"}
 LOGICAL_BACKUP_PROVIDER=${LOGICAL_BACKUP_PROVIDER:="s3"}
+DUMP_SIZE_COEFF=5
+ALL_DB_SIZE_QUERY="SELECT SUM(data_length + index_length) FROM information_schema.TABLES;"
+
+function estimate_size {
+  # Connects to MariaDB to calculate data size for S3 multipart upload optimization
+  mariadb -h "$MARIADB_HOST" -u "$MARIADB_USER" -p"$MARIADB_PASSWORD" \
+    --skip-ssl -Nsr -e "${ALL_DB_SIZE_QUERY}" < /dev/null
+}
 
 function dump {
   echo "Taking dump from ${MARIADB_HOST} using mariadb-dump for database ${MARIADB_DATABASE}" >&2
@@ -34,12 +42,29 @@ function generate_checksum {
   sha1sum "${FILE_PATH}" | tee "${CHECKSUM_FILE}"
 }
 
+function aws_upload {
+  local EXPECTED_SIZE="$1"
+  PATH_TO_BACKUP="s3://${LOGICAL_BACKUP_S3_BUCKET}/${CLUSTER_NAME}/${LOGICAL_BACKUP_S3_BUCKET_SCOPE_SUFFIX}/logical_backups/$(date +%s).sql.gz"
+
+  args=()
+  [[ -n "${EXPECTED_SIZE}" ]] && args+=("--expected-size=${EXPECTED_SIZE}")
+  [[ -n "${LOGICAL_BACKUP_S3_ENDPOINT}" ]] && args+=("--endpoint-url=${LOGICAL_BACKUP_S3_ENDPOINT}")
+  [[ -n "${LOGICAL_BACKUP_S3_REGION}" ]] && args+=("--region=${LOGICAL_BACKUP_S3_REGION}")
+
+  echo "Uploading dump to S3: ${PATH_TO_BACKUP}"
+  echo "${args[@]}"
+  aws s3 cp - "$PATH_TO_BACKUP" "${args[@]}"
+}
+
+
 if [ "$LOGICAL_BACKUP_PROVIDER" == "az" ]; then
   echo "Uploading to Azure Blob Storage..."
 else  
     dump 2> /tmp/dump_stderr.log | tee /tmp/raw_dump.sql | compress > /tmp/final_upload.sql.gz
     PIPELINE_STATUS=("${PIPESTATUS[@]}")
     generate_checksum /tmp/final_upload.sql.gz
+    local EXPECTED_SIZE=$(( $(estimate_size) / DUMP_SIZE_COEFF ))
+    cat /tmp/final_upload.sql.gz | aws_upload "${EXPECTED_SIZE}"
     sleep 1000
 
     [[ ${PIPELINE_STATUS[0]} != 0 || ${PIPELINE_STATUS[1]} != 0 || ${PIPELINE_STATUS[2]} != 0 ]] && (( ERRORCOUNT += 1 ))
